e54d865a4df46e964aa4e447241215c4fe63c9ed
AdrielModollo <adrielmodollo@outlook.com>
2023-04-11 23:00:01 -0300

    feat: movie route

--- src/modules/api/infra/http/routes/movies.routes.ts
+++ src/modules/api/infra/http/routes/movies.routes.ts
@@ -6,5 +6,6 @@ const moviesController = new MoviesController();
 
 moviesRouter.post("/", moviesController.createMovies);
 moviesRouter.get("/", moviesController.getAllMovies);
+moviesRouter.get("/film", moviesController.findByNameMovies);
 
 export default moviesRouter;
