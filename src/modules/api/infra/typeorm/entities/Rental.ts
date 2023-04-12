69e3ac06e15874687e4d196104a22c50d9686409
AdrielModollo <adrielmodollo@outlook.com>
2023-04-12 12:51:08 -0300

    feat: rental orm

---
+++ src/modules/api/infra/typeorm/entities/Rental.ts
@@ -0,0 +1,42 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+    DeleteDateColumn,
+    ManyToOne,
+    JoinColumn
+} from "typeorm";
+import User from "./User";
+
+@Entity("rentals")
+export default class Rental {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    movie_id: string;
+
+    @Column()
+    user_id: string;
+
+    @Column({ default: () => "now()" })
+    rental_date: Date;
+
+    @Column({ nullable: true })
+    return_date: Date;
+
+    @ManyToOne(() => User, user => user.rentals)
+    @JoinColumn({ name: "user_id" })
+    user: User;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    @DeleteDateColumn()
+    deleted_at: Date;
+}
