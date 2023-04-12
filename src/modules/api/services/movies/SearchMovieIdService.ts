cec4daa244164c491067716f91fa5fec10727a74
AdrielModollo <adrielmodollo@outlook.com>
2023-04-12 14:00:28 -0300

    feat: search movie id service

--- src/modules/api/services/movies/SearchMovieIdService.ts
+++ src/modules/api/services/movies/SearchMovieIdService.ts
@@ -3,14 +3,12 @@ import { HttpException, HttpStatusCode } from "../../../../shared/exceptions/Htt
 
 export async function SearchMovieIdService(movie_id) {
     const apiKey = process.env.TMDB_API_KEY;
-
     const apiUrl = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${apiKey}`;
 
-    const responseApi = await axios.get(apiUrl);
-
-    if (!responseApi.data) {
+    try {
+        const response = await axios.get(apiUrl);
+        return response.data;
+    } catch (error) {
         throw new HttpException(HttpStatusCode.NOT_FOUND, 'Movie not found.');
     }
-
-    return responseApi.data;
 }
