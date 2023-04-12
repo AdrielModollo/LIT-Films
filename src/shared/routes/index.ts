896f254345abd128789efb90d2511568a3961767
AdrielModollo <adrielmodollo@outlook.com>
2023-04-11 22:43:18 -0300

    feat: add movies routes

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -4,6 +4,7 @@ import YAML from 'yamljs';
 import path from 'path';
 import usersRouter from "../../modules/api/infra/http/routes/users.routes";
 import authenticateRouter from "../../modules/api/infra/http/routes/auth.routes";
+import moviesRoutes from "../../modules/api/infra/http/routes/movies.routes";
 import authenticate from "../../modules/api/infra/http/middlewares/authenticateMiddleware";
 import { notFoundHandler } from "../../modules/api/infra/http/middlewares/errorHandlerMiddleware";
 
@@ -15,6 +16,7 @@ routes.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 routes.use('/users', authenticateRouter);
 routes.use(authenticate);
 routes.use('/users', usersRouter);
+routes.use('/movies', moviesRoutes);
 routes.use(notFoundHandler);
 
 export default routes;
