47722eda397e1702af3b968a4cbe0eee485b74f7
AdrielModollo <adrielmodollo@outlook.com>
2023-04-12 00:01:17 -0300

    feat: update movie routes

--- src/modules/api/infra/http/routes/movies.routes.ts
+++ src/modules/api/infra/http/routes/movies.routes.ts
@@ -7,6 +7,7 @@ const moviesController = new MoviesController();
 moviesRouter.post("/", moviesController.createMovies);
 moviesRouter.get("/", moviesController.getAllMovies);
 moviesRouter.get("/film", moviesController.findByNameMovies);
+moviesRouter.patch("/", moviesController.updateMovies);
 moviesRouter.delete("/", moviesController.softDeleteMovie);
 
 export default moviesRouter;
