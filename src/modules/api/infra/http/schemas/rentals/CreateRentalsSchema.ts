6689b289b3aee1aca7ec61478733b11c755df205
AdrielModollo <adrielmodollo@outlook.com>
2023-04-12 12:50:53 -0300

    feat: rental schema

---
+++ src/modules/api/infra/http/schemas/rentals/CreateRentalsSchema.ts
@@ -0,0 +1,8 @@
+import Joi from 'joi';
+
+export const createRentalSchema = Joi.object({
+    movie_id: Joi.number().required(),
+    user_id: Joi.string().required(),
+    rental_date: Joi.date().required(),
+    return_date: Joi.date().optional(),
+});
