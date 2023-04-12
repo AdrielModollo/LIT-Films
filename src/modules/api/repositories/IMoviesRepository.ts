76a4bb4a5f32c4844671cd8356e28a66debaaae7
AdrielModollo <adrielmodollo@outlook.com>
2023-04-12 00:24:55 -0300

    fix: movie repository

--- src/modules/api/repositories/IMoviesRepository.ts
+++ src/modules/api/repositories/IMoviesRepository.ts
@@ -2,7 +2,7 @@ import Movie from '../infra/typeorm/entities/Movie';
 import { ICreateMovieDTO } from '../dtos/movies/ICreateMovieDTO';
 import { IUpdateMovieDTO } from '../dtos/movies/IUpdateMovieDTO';
 
-export default interface IUsersRepository {
+export default interface IMoviesRepository {
     createMovies(data: ICreateMovieDTO): Promise<Movie>;
     getAllMovies(): Promise<Movie[]>;
     findByName(email: string): Promise<Movie | undefined>;
