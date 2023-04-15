102bdc9f6f232a8995bdad5db90bebe40f79907d
AdrielModollo <adrielmodollo@outlook.com>
2023-04-15 11:42:52 -0300

    test should create a new rental

--- __tests__/unit/modules/api/services/rentals/CreateRentalsService.spec.ts
+++ __tests__/unit/modules/api/services/rentals/CreateRentalsService.spec.ts
@@ -1,16 +1,19 @@
 import { CreateRentalService } from '../../../../../../src/modules/api/services/rentals/CreateRentalService';
 import { LeaseTermValidationService } from '../../../../../../src/modules/api/services/rentals/LeaseTermValidationService';
+import IRentalsRepository from '../../../../../../src/modules/api/repositories/IRentalsRepository';
 import RentalsRepositoryMock, { mockMovieData } from '../../../../repositories/MockRentalsRepository';
+import IUsersRepository from '../../../../../../src/modules/api/repositories/IUsersRepository';
 import UsersRepositoryMock from '../../../../repositories/MockUsersRepository';
 import { IRequestRental } from '../../../../../../src/modules/api/dtos/rentals/IRequestRentalDTO';
 import { HttpException, HttpStatusCode } from '../../../../../../src/shared/exceptions/HttpException';
+import { SearchMovieIdService } from '../../../../../../src/modules/api/services/movies/SearchMovieIdService';
 
 describe('CreateRentalService', () => {
     let createRentalService: CreateRentalService;
     let leaseTermValidationService: LeaseTermValidationService;
     let rentalsRepositoryMock: RentalsRepositoryMock;
     let usersRepositoryMock: UsersRepositoryMock;
-    let searchMovieIdService;
+    let searchMovieIdService: SearchMovieIdService;
 
     beforeEach(() => {
         rentalsRepositoryMock = new RentalsRepositoryMock();
@@ -25,6 +28,7 @@ describe('CreateRentalService', () => {
             rentalsRepositoryMock,
             leaseTermValidationService,
             usersRepositoryMock,
+            searchMovieIdService,
         );
     });
 
@@ -41,6 +45,40 @@ describe('CreateRentalService', () => {
             await expect(createRentalService.execute(data)).rejects.toThrow(
                 new HttpException(HttpStatusCode.NOT_FOUND, 'User Not Found!')
             );
+        });
+    });
+
+    describe('when user exists', () => {
+        it('should create a new rental', async () => {
+            const user = {
+                id: "5fd7e009-14c2-4dd5-86e0-bb54677eb60f",
+                name: "John Doe",
+                email: "johndoe@example.com",
+                password: "$2a$08$F6p3iClwJRyu4FURz4YFmOFnrhDa57IaeNLA8gbhhKp.ElbxHZyla",
+                rentals: [],
+                created_at: new Date(),
+                updated_at: new Date(),
+                deleted_at: null,
+            };
+
+            const data: IRequestRental = {
+                user_id: user.id,
+                movie_id: 1102706,
+                rental_date: new Date(),
+            };
+
+            jest.spyOn(usersRepositoryMock, 'findById').mockResolvedValueOnce(user);
+
+            const createRentalMock = jest.spyOn(rentalsRepositoryMock, 'createRental');
+            createRentalMock.mockImplementation(jest.fn());
+
+            const newRental = await createRentalService.execute(data);
+
+            expect(createRentalMock).toHaveBeenCalledWith({
+                user_id: data.user_id,
+                movie_id: data.movie_id,
+                rental_date: data.rental_date,
+            });
         });
     });
 });
--- __tests__/unit/repositories/MockAuthenticateRepository.ts
+++ __tests__/unit/repositories/MockAuthenticateRepository.ts
@@ -3,7 +3,7 @@ import IAuthenticateRepository from "../../../src/modules/api/repositories/IAuth
 class MockAuthenticateRepository implements IAuthenticateRepository {
     private users = [
         {
-            id: "user-id",
+            id: "5fd7e009-14c2-4dd5-86e0-bb54677eb60f",
             name: "John Doe",
             email: "johndoe@example.com",
             password: "$2a$08$F6p3iClwJRyu4FURz4YFmOFnrhDa57IaeNLA8gbhhKp.ElbxHZyla",
@@ -13,6 +13,11 @@ class MockAuthenticateRepository implements IAuthenticateRepository {
             deleted_at: null,
         }
     ];
+
+    async findById(id: string) {
+        const user = this.users.find(user => user.id === id);
+        return user;
+    }
 
     async findByEmail(email: string) {
         const user = this.users.find((user) => user.email === email);
